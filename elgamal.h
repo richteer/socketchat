7150568238340de904d82d6392b2e5146821cf66
Ethan Miller <cactusm123@gmail.com>
2013-12-04 15:31:42 -0500

    Function prototypes

--- elgamal.h
+++ elgamal.h
@@ -3,10 +3,15 @@
 
 typedef struct eg_pub_key_s
 {
-	mpz_t p;
-	mpz_t n;
+	mpz_t p;	// k-bit safe prime
+	mpz_t g;	// generator for p
+	mpz_t beta;	// g^a, where a is the private exponent
 } eg_pub_key_t;
 
 typedef mpz_t eg_priv_key_t;
+
+void eg_gen_key( eg_pub_key_t pub, eg_priv_key_t priv, int bitlength );
+void eg_encrypt( eg_pub_key_t pub, char *ptext, char *ctext );
+void eg_decrypt( eg_pub_key_t pub, eg_priv_key_t priv, char *ctext, char *ptext );
 
 #endif
