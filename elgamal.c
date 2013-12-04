8403c4e14237a79298c8751b332e1214ea5253c2
Ethan Miller <cactusm123@gmail.com>
2013-12-04 16:35:03 -0500

    Tested gen_safe_prime

--- elgamal.c
+++ elgamal.c
@@ -11,12 +11,26 @@
 #include <stdlib.h>
 #include <gmp.h>
 
-
+#include "number.h"
 #include "elgamal.h"
+
+void eg_init_key( eg_pub_key_t pub, eg_priv_key_t priv )
+{
+	mpz_init(pub.p);
+	mpz_init(pub.g);
+	mpz_init(pub.beta);
+
+	if (NULL != (void *) priv) {
+		mpz_init(priv);
+	}
+}
 
 void eg_gen_key( eg_pub_key_t pub, eg_priv_key_t priv, int bitlength )
 {
-	// TODO
+	mpz_t q; mpz_init(q);
+	gmp_randstate_t r_state;
+	get_rand_seed(r_state);
+
 }
 
 void eg_encrypt( eg_pub_key_t pub, char *ptext, char *ctext )
--- test_number.c
+++ test_number.c
@@ -9,10 +9,14 @@ int main()
 	mpz_init(a);
 	gmp_randstate_t r_state;
 	get_rand_seed(r_state);
-	gen_number(10, a, r_state);
+	gen_number(a, 10, r_state);
 	gmp_fprintf(stderr,"Random number: %Zd\n", a);
-	gen_prime(10, a, r_state);
+	gen_prime(a, 10, r_state);
 	gmp_fprintf(stderr,"Random prime: %Zd\n", a);
-	
+	gen_safe_prime(a, 5, r_state);
+	gmp_fprintf(stderr, "Random safe prime: %Zd\n", a);
+
+	mpz_clear(a);
+	gmp_randclear(r_state);
 	return 0;
 }
