c9bb81602bd4a8ad335a0cd4954d1ca761195690
richteer <richteer@clarkson.edu>
2013-12-04 15:34:03 -0500

    Removed server, reworked client, fixed makefile

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
 CC=gcc
 CFLAGS=-Wall
 
-all: network.o
+all: network.o tests
 
 tests: test_number.c number.h number.c
 	@echo "Building test_number"
-	${CC} ${CFlAGS} test_number.c number.c -lgmp
+	${CC} ${CFLAGS} test_number.c number.c -lgmp
 
 network.o: network.c network.h
 	@echo "Building the Network Library"
 	${CC} ${CFLAGS} -c $< -o $@
 
+client: client.c
+	@echo "Building the client"
+	${CC} ${CFLAGS} $< -o $@
+
 clean: 
-	rm -rf *.o a.out
+	rm -rf *.o a.out client
--- client.c
+++ client.c
@@ -4,7 +4,14 @@
 
 #include "network.h"
 
+#define ARG_CONNECT (0x1<<1)
+#define ARG_LISTEN  (0x1<<2)
+#define ARG_VERBOSE (0x1<<3)
+
 unsigned arg_flags = 0;
+char* conip = NULL;
+char* conpo = NULL;
+
 
 int handle_args(int,char**);
 void show_help(void);
@@ -14,15 +21,15 @@ void handle_sigint(int x) { net_close(); exit(0); }
 int main(int argc, char **argv)
 {
 	if ((argc != 1) && handle_args(argc,argv)) {
-		fprintf("Error parsing arguments\n");
+		//fprintf("Error parsing arguments\n");
+		show_help();
 		exit(1);
 	}
 	
 	printf("Server started, hit ^C to exit\n");
-	if (0 != net_init(NULL,"12345")) {
-		fprintf("Could not initialize the network!\n");
-		exit(2);
-	}
+
+
+
 	signal(SIGINT,handle_sigint);
 
 	/* DO CLIENT-LIKE STUFF HERE */
@@ -35,8 +42,20 @@ int handle_args(int argc, char** argv)
 	int i, ret = 0;
 	for (i = 1; i < argc; i++) {asdjkalsdjkhashkjdakjlhsdkjh
 		switch(argv[i][1]) {
-			case 'v': arg_flags |= ARG_VERBOSE; break;
 			case 'h': show_help(); exit(2); 	break;
+			case 'c': 
+				arg_flags |= ARG_CONNECT; 
+				if (i+2 >= argc) {ret = -1; break;}
+				conip = argv[++i];
+				conpo = argv[++i];
+				break;
+			case 'l':
+			case 'w':
+				flags |= ARG_WAIT;
+				if (i+1 >= argc) {ret = -1; break;}
+				conpo = argv[++i];
+				break;
+			case 'v': arg_flags |= ARG_VERBOSE; break;
 			default: ret = -1;
 		}
 	}
@@ -46,6 +65,11 @@ int handle_args(int argc, char** argv)
 
 void show_help()
 {
-	fprintf(stderr,"Not implemented yet!\n");
+	fprintf(stderr,"Usage: %s [OPTION] [OPTION ARGS]\n",argv[0]);
+	fprintf(stderr,"Options:\n");
+	fprintf(stderr,"\t-h\t\tShow this help\n");
+	fprintf(stderr,"\t-c <ip> <port>\tConnect to another user with supplied ip and port\n");
+	fprintf(stderr,"\t-w <port>\n");
+	fprintf(stderr,"\t-l <port>\tListen on the supplied port for a connection\n");
 	return;
 }
