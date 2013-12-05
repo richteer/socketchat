698d60c97244f470e5d4ac020d882b7c6592fe6f
Ethan Miller <cactusm123@gmail.com>
2013-12-04 21:02:36 -0500

    New message struct for ElGamal

--- src/crypto/elgamal.h
+++ src/crypto/elgamal.h
@@ -10,11 +10,17 @@ typedef struct eg_pub_key_s
 
 typedef mpz_t eg_priv_key_t;
 
+typedef struct eg_message_s
+{
+	mpz_t mask;
+	mpz_t message;
+} eg_message_t;
+
 // To ignore an arg, pass NULL
 void eg_init_key( eg_pub_key_t *pub, eg_priv_key_t priv );
 
 void eg_gen_key( eg_pub_key_t *pub, eg_priv_key_t priv, int bitlength );
-void eg_encrypt( eg_pub_key_t pub, mpz_t plain, mpz_t  cipher);
-void eg_decrypt( eg_pub_key_t pub, eg_priv_key_t priv, mpz_t cipher, mpz_t plain );
+void eg_encrypt( eg_pub_key_t pub, mpz_t plain, eg_message_t cipher);
+void eg_decrypt( eg_pub_key_t pub, eg_priv_key_t priv, eg_message_t cipher, mpz_t plain );
 
 #endif
