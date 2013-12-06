cb75e6be3d78078441597276bcdf4e596aadf18b
richteer <richteer@clarkson.edu>
2013-12-05 20:41:08 -0500

    Fixed the bug in the aes library

--- src/crypto/aes.c
+++ src/crypto/aes.c
@@ -1,21 +1,23 @@
 #include <mcrypt.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+//#include <stdio.h>
 
 #define BUF_SIZE 256
 
 MCRYPT cr_td;
 char cr_key[17] = {0};
 char cr_iv[17] = {0};
+int cr_blocksize = -1;
+
 
 int cr_init(char* key, char* iv)
 {
 	strncpy(cr_key,key,16);
 	strncpy(cr_iv,iv,16);
-	//cr_td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
+	cr_td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
+	cr_blocksize = mcrypt_enc_get_block_size(cr_td);
 
-	/* TODO: Fix this */
 	return  0;
 }
 
@@ -24,24 +26,24 @@ void cr_close()
 	mcrypt_module_close(cr_td);
 }
 
-int cr_encrypt(char* buffer)
+int cr_encrypt(char* buffer, size_t bufsize)
 {
-	cr_td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
+	if (bufsize % cr_blocksize) return -1;
+
 	mcrypt_generic_init(cr_td,cr_key,16,cr_iv);
 	mcrypt_generic(cr_td,buffer,BUF_SIZE);
 	mcrypt_generic_deinit(cr_td);
-	mcrypt_module_close(cr_td);
 
 	return 0;
 }
 
-int cr_decrypt(char* buffer)
+int cr_decrypt(char* buffer, size_t bufsize)
 {
-	cr_td = mcrypt_module_open("rijndael-128", NULL, "cbc", NULL);
+	if (bufsize % cr_blocksize) return -1;
+
 	mcrypt_generic_init(cr_td,cr_key,16,cr_iv);
-	mcrypt_generic(cr_td,buffer,BUF_SIZE);
+	mdecrypt_generic(cr_td,buffer,BUF_SIZE);
 	mcrypt_generic_deinit(cr_td);
-	mcrypt_module_close(cr_td);
 
 	return 0;
 }
--- src/crypto/aes.h
+++ src/crypto/aes.h
@@ -1,4 +1,4 @@
 int cr_init(char* key,char *iv);
 int cr_close();
-int cr_encrypt(char* buffer);
-int cr_decrypt(char* buffer);
+int cr_encrypt(char* buffer, int bufsize);
+int cr_decrypt(char* buffer, int bufsize);
